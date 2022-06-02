2ad695c9d49a2c3562d525f742006801c9c1e232
jelle-vdp <40336977+jelle-vdp@users.noreply.github.com>
2022-06-02 10:40:56 +0200

    added description + doubleclick event to the shrimp game

--- script.js
+++ script.js
@@ -38,7 +38,6 @@ btn.removeAttribute("href");
 btn.addEventListener("click", () => {
     const ogText = document.querySelector(".cards .cards .col-lg-3:nth-child(7) .card-text");
     const ogChars = ogText.innerText.split(" ");
-    console.log(ogChars);
     ogText.innerText = "";
     ogChars.forEach(char => {
         let randomColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
@@ -51,6 +50,12 @@ const generateRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
+const moveRandomly = (el , moveX, moveY) => {
+    el.style.top = `${+el.style.top.substr(0, el.style.top.length - 2)  + moveY}px`;
+    el.style.left = `${+el.style.left.substr(0, el.style.left.length - 2) + moveX}px`;
+}
+
+
 const shrimpUrlArr = [
     "https://www.bittentoast.com/rpgicons/fant_env/Shrimp.png",
     "https://icons.iconarchive.com/icons/google/noto-emoji-animals-nature/256/22301-shrimp-icon.png",
@@ -58,23 +63,32 @@ const shrimpUrlArr = [
     "https://icons-for-free.com/download-icon-shrimp-131983752943175303_256.png",
     "https://styles.redditmedia.com/t5_2v6xy/styles/communityIcon_o60uuk98pwm11.png",
     "https://www.shareicon.net/data/256x256/2016/05/04/760011_animal_512x512.png",
-    "https://images.vexels.com/media/users/3/253666/isolated/lists/6ea5a2437b50ab94d9a2a9c5caccba9d-shrimply-the-best-shrimp-quote-color-stroke.png",
-
+    "https://images.vexels.com/media/users/3/253666/isolated/lists/6ea5a2437b50ab94d9a2a9c5caccba9d-shrimply-the-best-shrimp-quote-color-stroke.png"
 ];
 
 document.querySelector(".cards .cards .col-lg-3:nth-child(8) .card-body").innerHTML = "<button>Click me to open the shrimp game</button>";
 
 document.querySelector("button").addEventListener("click", () => {
+    const shrimpSection = document.createElement("section");
+    shrimpSection.classList.add("container");
+    shrimpSection.style.marginTop = "3rem";
+    
+    const shrimpExplanation = document.createElement("p");
+    shrimpExplanation.innerText = "Click inside the box to spawn a random shrimp. Click on one of the shrimps to make it spin around. Doubleclick on the shrimp to move it inside the box";
+
     const shrimpContainer = document.createElement("div");
     shrimpContainer.style.border = "1px solid #333";
     shrimpContainer.style.height = "500px";
-    shrimpContainer.style.marginTop = "2rem";
+    shrimpContainer.style.marginTop = "1rem";
     shrimpContainer.style.position = "relative";
     shrimpContainer.style.overflow = "hidden";
-    shrimpContainer.classList.add("container");
     shrimpContainer.classList.add("shrimp-game");
+
+    shrimpSection.appendChild(shrimpExplanation);
+    shrimpSection.appendChild(shrimpContainer);
+
     const lastElement = document.querySelector(".container.cards");
-    lastElement.parentNode.insertBefore(shrimpContainer, lastElement.nextSibling);
+    lastElement.parentNode.insertBefore(shrimpSection, lastElement.nextSibling);
     const shrimpGameContainerDOM = document.querySelector(".shrimp-game");
     shrimpGameContainerDOM.addEventListener("click", (e) => {
         if (e.target.tagName === "DIV"){
@@ -83,12 +97,15 @@ document.querySelector("button").addEventListener("click", () => {
             shrimpImg.style.position = "absolute";
             shrimpImg.style.width = "100px";
             shrimpImg.style.transition = "0.5s ease-in-out";
-            shrimpImg.style.top = `${generateRandomNumber(-100, 600)}px`;
+            shrimpImg.style.top = `${generateRandomNumber(-50, 550)}px`;
             let widthShrimpContainer = shrimpGameContainerDOM.getBoundingClientRect().right - shrimpGameContainerDOM.getBoundingClientRect().left;
-            shrimpImg.style.left = `${generateRandomNumber(-100, widthShrimpContainer + 100)}px`;
+            shrimpImg.style.left = `${generateRandomNumber(-50, widthShrimpContainer + 50)}px`;
             shrimpGameContainerDOM.appendChild(shrimpImg);
         } else if (e.target.tagName === "IMG"){
-            e.target.style.transform = "rotate(360deg)"
-        }
+            e.target.style.transform = "rotate(360deg)";
+            e.target.addEventListener("dblclick", () => {
+                moveRandomly(e.target, generateRandomNumber(-100, 100), generateRandomNumber(-100, 100));
+            })
+        };
     });
 });
